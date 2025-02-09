16389733362a2caa6b5cd69d758d7c4c037c33e8
Muhammad Khotib <mkhotib20@gmail.com>
2025-02-09 12:25:31 +0700

    [FIX][CORE] Fixing build tsc with aliases

--- apps/frontend-repo/hooks/useSocialState.ts
+++
@@ -1,8 +0,0 @@
-import { useAppSelector } from '@/store/hooks';
-
-// Simplify get user data selector
-const useSocialState = () => {
-  return useAppSelector((state) => state.social);
-};
-
-export default useSocialState;
